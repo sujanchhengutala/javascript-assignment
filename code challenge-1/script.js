b0d5916fd131ad6c86a40d8e37c59eb664dca2a9
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-17 09:39:46 +0545

    challenge-1

---
+++ code challenge-1/script.js
@@ -0,0 +1,30 @@
+let markMass;
+let johnMass;
+let markHeight; 
+let johnHeight;
+let markBMI; 
+let johnBMI;
+markMass = 78;
+johnMass = 92;
+markHeight = 1.69;
+johnHeight = 1.95;
+
+markBMI = (markMass/(markHeight*markHeight));
+johnBMI = (johnMass/(johnHeight*johnHeight));
+
+console.log(markBMI , johnBMI);
+
+//let markBiggerBMI = markBMI > johnBMI;
+//if(markBMI > johnBMI){
+ // console.log(mark BMI is highter than john);
+//}
+//else{
+ // console.log(john BMI is highter than john);
+
+//}
+if(markBMI>johnBMI){
+  console.log(markbig);
+}
+else{
+  console.log(johnbig);
+}
