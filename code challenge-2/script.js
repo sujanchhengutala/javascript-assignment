bd7ceb53b84bad9bf741263931ccbd2315d8b067
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-17 09:42:19 +0545

    challenge-2

---
+++ code challenge-2/script.js
@@ -0,0 +1,8 @@
+let markBMI = 27.309968138370508;
+let johnBMI = 24.194608809993426;
+if(markBMI > johnBMI){
+  console.log(`mark has higher BMI(27.309968138370508)  than jhon (24.194608809993426)`);
+}
+else{
+  console.log(`john has higher BMI (24.194608809993426) than mark(27.309968138370508)`);
+}
